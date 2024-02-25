d4ba291e4f58d545f2b9da307501a375f7370489
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-02-25 14:29:18 -0300

    scripts

---
+++ 1_screen.sh
@@ -0,0 +1,2 @@
+#!bin/sh
+xrandr --output HDMI-A-0 --auto --output DVI-D-0 --off
---
+++ 2_screens.sh
@@ -0,0 +1,2 @@
+#!bin/sh
+xrandr --output DVI-D-0 --auto && xrandr --output DVI-D-0 --left-of HDMI-A-0 && xrandr --output DVI-D-0 --pos 0x312
