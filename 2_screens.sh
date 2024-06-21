af34905eb29c787d47f5baf3578eea565dad1f63
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-06-20 22:33:17 -0300

    adjusting resolution

--- 2_screens.sh
+++ 2_screens.sh
@@ -1,2 +1,3 @@
 #!bin/sh
-xrandr --output DVI-D-0 --auto && xrandr --output DVI-D-0 --left-of HDMI-A-0 && xrandr --output DVI-D-0 --pos 0x312
+xrandr --output DVI-D-0 --mode 1280x720
+xrandr --output DVI-D-0 --left-of HDMI-A-0 && xrandr --output DVI-D-0 --pos 0x312
