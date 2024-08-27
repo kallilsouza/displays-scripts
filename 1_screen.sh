7e45564bf235c947aaf805941d0397f2bcb00774
Kallil Souza dos Santos <kallilsouza@hotmail.com>
2024-08-27 06:52:12 -0300

    changing HDMI output name

--- 1_screen.sh
+++ 1_screen.sh
@@ -1,3 +1,3 @@
 #!bin/sh
-xrandr --output HDMI-A-0 --auto --output DVI-D-0 --off
-xrandr --output HDMI-A-0 --mode 2560x1080 --rate 74.99
+xrandr --output HDMI-0 --auto --output DVI-D-0 --off
+xrandr --output HDMI-0 --mode 2560x1080 --rate 74.99
--- 2_screens.sh
+++ 2_screens.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 xrandr --output DVI-D-0 --mode 1280x720
-xrandr --output HDMI-A-0 --mode 2560x1080 --rate 74.99
-xrandr --output DVI-D-0 --left-of HDMI-A-0 && xrandr --output DVI-D-0 --pos 0x312
+xrandr --output HDMI-0 --mode 2560x1080 --rate 74.99
+xrandr --output DVI-D-0 --left-of HDMI-0 && xrandr --output DVI-D-0 --pos 0x312
